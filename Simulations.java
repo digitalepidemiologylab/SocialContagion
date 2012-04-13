f1d7e504330772d71232522dda3813e5294356f0
Ells <ells.cubed@gmail.com>
2012-04-13 11:35:59 -0400

    Revert "Revert "theClusterHeatmaps generates: predicted + simulated outbreak size & cluster count""
    This reverts commit 0477cf435a947e899a03f1b52598441ebc34ad18.

--- Simulations.java
+++ Simulations.java
@@ -42,6 +42,7 @@ public class Simulations {
     int onlySocialEdges = 0;
     int mixedSocialEdges = 0;
     int simulSocialEdges = 0;
+    double simulatedAverageOutbreak = 0;
 
     public static void main(String[] args) {
         Simulations simulation = new Simulations();
@@ -55,6 +56,7 @@ public class Simulations {
         this.removeVaccinated();
         this.clusters();
         this.predictOutbreakSize();
+        this.multiBioSims();
     }
 
     public void recordGraph() {
@@ -134,15 +136,10 @@ public class Simulations {
         }
     }
 
-    public void predictVsimulate() {
+    public void multiBioSims() {
         outbreakSizeList = new ArrayList<Integer>();
-        this.initGraph();
-        this.runSocialTimesteps();
-        this.removeVaccinated();
-        this.clusters();
 
-
-        int simCount = 100;
+        int simCount = 20;
         for (int i = 0; i < simCount; i++) {
             this.outbreakSize = 0;
             this.runBiologicalTimesteps();
@@ -154,12 +151,12 @@ public class Simulations {
         for (int i = 0; i < simCount; i++) {
             outbreakSum = outbreakSum + outbreakSizeList.get(i);
         }
-        double simulatedAverageOutbreak = outbreakSum/simCount;
+        this.simulatedAverageOutbreak = outbreakSum/simCount;
 
-        double ratioSimulatedTOPredicted = simulatedAverageOutbreak/this.predictedOutbreakSize;
+    }
 
-        System.out.println(SimulationSettings.getInstance().getRewiringProbability() + "," + ratioSimulatedTOPredicted);
-
+    public double getSimulatedAverageOutbreak() {
+        return this.simulatedAverageOutbreak;
     }
 
     public double getFractionHealthStatus(int status) {
